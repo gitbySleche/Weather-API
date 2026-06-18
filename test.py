58bc196c3b94359ff7685b0674304e0023e56bad
Miguel <miguel.goncalves.p.leite@gmail.com>
2026-06-18 11:08:22 +0100

    Stop tracking test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import redis
-
-r = redis.Redis(host='localhost', port=6379, decode_responses=True)
-
-r.set('test_key', 'hello from python', ex=10)  # expires in 10 seconds
-print(r.get('test_key'))
